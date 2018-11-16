ce9953b4d3beb40647f88b0c7c28c37355e0d34c
alex <alex@192.168.1.57>
2018-11-16 16:08:18 -0600

    new resorce alien

---
+++ src/main/java/com/proyecto/com/telusko/Alien.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proyecto.com.telusko;
+
+//import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+
+/**
+ *
+ * @author alex
+ */
+
+@JsonInclude(Include.NON_EMPTY)
+//@XmlRootElement
+public class Alien {
+    
+    private String name ;
+    private int points;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }    
+}
